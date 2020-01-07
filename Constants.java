aef2a9ffdbf9b04eab8a0dc9bb1661608b546852
Jean-Pierre Lerbscher <jp.lerbscher@loxodata.com>
2020-01-07 14:03:03 +0100

    Init constants.java files

---
+++ Constants.java
@@ -0,0 +1,32 @@
+
+/**
+ * Constants
+ *
+ */
+public final class Constants {
+
+  public static final String SOMETHING_HERE = new String("something");
+
+  public String retour1() {
+    return new String("test");
+  }
+
+  private String retour2() {
+    boolean var1 = true;
+    boolean var2 = true;
+    if (var1) {
+      if(var2) {
+        return "test2";
+      }
+    }
+    return "test";
+  }
+  
+  public boolean checkIfToto(String s) {
+    return s.equals("test");
+  }
+
+  public String convert(int myInteger) {
+      return new Integer(myInteger).toString(); 
+  }
+}
